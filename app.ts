11cbcf46de95ae2c9dc152afd17b63f9aa15f764
onishi taisei <taisei.developer@gmail.com>
2023-01-05 22:43:26 +0900

    Tuple型について学習

--- app.ts
+++ app.ts
@@ -8,10 +8,16 @@
 // };
 
 {
-  const person = {
+  const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+  } = {
     name: "taisei",
     age: 25,
     hobbies: ["Sports", "Cooking"],
+    role: [2, "author"],
   };
 
   let favoriteActivities: string[];
