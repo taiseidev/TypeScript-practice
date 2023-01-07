172e92f0dede7d410875fd66a8503248783ad08f
onishi taisei <taisei.developer@gmail.com>
2023-01-07 15:56:07 +0900

    unknown型について学習

--- app.ts
+++ app.ts
@@ -1,25 +1,13 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
 
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-
-function addAndHadle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-addAndHadle(10, 20, (result) => {
-  console.log(result);
-});
-
-printResult(add(5, 12));
-console.log(printResult(add(5, 12))); // 何も返さないため出力はundefinedとなる
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(8, 8));
+/// unknown型はany型と同じでどんな型でも入れることができる
+/// unknown型はany型よりは良い選択肢
+/// 書き換えをする際に型チェックを行う必要がある
