b38aa1c8fa118ad8950c78c1e179f036157f5a91
onishi taisei <taisei.developer@gmail.com>
2023-01-07 16:00:23 +0900

    never型について学習

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: " + num);
+/// unknown型はany型と同じでどんな型でも入れることができる
+/// unknown型はany型よりは良い選択肢
+/// 書き換えをする際に型チェックを行う必要がある
+/// 値を絶対に返さないことがわかっている場合はnever型を指定する
+function generageError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addAndHadle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-addAndHadle(10, 20, function (result) {
-    console.log(result);
-});
-printResult(add(5, 12));
-console.log(printResult(add(5, 12))); // 何も返さないため出力はundefinedとなる
-var combineValues;
-combineValues = add;
-console.log(combineValues(8, 8));
+generageError("エラーが発生しました", 500);
