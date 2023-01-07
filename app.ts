d6e795795943fb0951ca989862cdd1e142079dc3
onishi taisei <taisei.developer@gmail.com>
2023-01-07 15:41:00 +0900

    void型とundefined型について学習

--- app.ts
+++ app.ts
@@ -1,31 +1,10 @@
-function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
-) {
-  let result: string | number;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // 適切な動作ではなかったのでコメントアウト
-  //   if (resultConversion === "as-number") {
-  //     return +result;
-  //   } else {
-  //     return result.toString();
-  //   }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-
-const combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+printResult(add(5, 12));
+console.log(printResult(add(5, 12))); // 何も返さないため出力はundefinedとなる
