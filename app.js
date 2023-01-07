b12fdb1c00c842b4b9e15660535e334a392a1f6b
onishi taisei <taisei.developer@gmail.com>
2023-01-07 15:49:54 +0900

    callbackについて学習

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result: " + num);
 }
+function addAndHadle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHadle(10, 20, function (result) {
+    console.log(result);
+});
 printResult(add(5, 12));
 console.log(printResult(add(5, 12))); // 何も返さないため出力はundefinedとなる
 var combineValues;
--- app.ts
+++ app.ts
@@ -6,6 +6,15 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHadle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHadle(10, 20, (result) => {
+  console.log(result);
+});
+
 printResult(add(5, 12));
 console.log(printResult(add(5, 12))); // 何も返さないため出力はundefinedとなる
 
