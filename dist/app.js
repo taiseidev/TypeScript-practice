b845a9077e9de3e05da434d85e5c9d200c1cb2f7
onishi taisei <taisei.developer@gmail.com>
2023-01-15 22:41:08 +0900

    TypeGuardについて学習

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,53 @@
 "use strict";
-const e1 = {
-    name: "Max",
-    privileges: ["create-server"],
-    startDate: new Date(),
-};
+{
+    const e1 = {
+        name: "Max",
+        privileges: ["create-server"],
+        startDate: new Date(),
+    };
+    function add(a, b) {
+        if (typeof a === "string" || typeof b === "string") {
+            return a.toString() + b.toString();
+        }
+        return a + b;
+    }
+    add(12, 12);
+    function printEmployeeInformation(emp) {
+        console.log(emp.name);
+        if ("privileges" in emp) {
+            console.log("Privilages: " + emp.privileges);
+        }
+        if ("startDate" in emp) {
+            console.log("Privilages: " + emp.startDate);
+        }
+    }
+    printEmployeeInformation(e1);
+    printEmployeeInformation({
+        name: "Manu",
+        startDate: new Date(),
+    });
+    class Car {
+        drive() {
+            console.log("運転中...");
+        }
+    }
+    class Truck {
+        drive() {
+            console.log("トラック運転中...");
+        }
+        loadCargo(amount) {
+            console.log("荷物を乗せています..." + amount);
+        }
+    }
+    const v1 = new Car();
+    const v2 = new Truck();
+    function useVehicle(vehicle) {
+        vehicle.drive();
+        if (vehicle instanceof Truck) {
+            vehicle.loadCargo(1000);
+        }
+    }
+    useVehicle(v1);
+    useVehicle(v2);
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -10,31 +10,96 @@
 
 // interface ElevatedEmployee extends Employee, Admin {}
 
-type Admin = {
-  name: string;
-  privileges: string[];
-};
+{
+  type Admin = {
+    name: string;
+    privileges: string[];
+  };
 
-type Employee = {
-  name: string;
-  startDate: Date;
-};
+  type Employee = {
+    name: string;
+    startDate: Date;
+  };
 
-// 交差型
-// 複数の型を結合
-// どんな型についても使うことができる
-type ElevatedEmployee = Admin & Employee;
+  // 交差型
+  // 複数の型を結合
+  // どんな型についても使うことができる
+  type ElevatedEmployee = Admin & Employee;
 
-const e1: ElevatedEmployee = {
-  name: "Max",
-  privileges: ["create-server"],
-  startDate: new Date(),
-};
+  const e1: ElevatedEmployee = {
+    name: "Max",
+    privileges: ["create-server"],
+    startDate: new Date(),
+  };
 
-type Combinables = string | number;
-type Numeric = number | boolean;
+  type Combinables = string | number;
+  // type Numeric = number | boolean;
 
-// 交差型は交差している型を作るためunion型で交差型がを作成する場合は
-// 下記のように交差しているnumber型のみが結合される
-// オブジェクト型の場合は各プロパティが使用できるようになる
-type Universal = Combinables & Numeric;
+  // 交差型は交差している型を作るためunion型で交差型がを作成する場合は
+  // 下記のように交差しているnumber型のみが結合される
+  // オブジェクト型の場合は各プロパティが使用できるようになる
+  // type Universal = Combinables & Numeric;
+
+  function add(a: Combinables, b: Combinables) {
+    if (typeof a === "string" || typeof b === "string") {
+      return a.toString() + b.toString();
+    }
+    return a + b;
+  }
+
+  add(12, 12);
+
+  type UnknownEmployee = Employee | Admin;
+
+  function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log(emp.name);
+    // カスタム型はjavascriptでは認識されないため、typeof emp === 'Admin'
+    // といった書き方はできない
+    if ("privileges" in emp) {
+      console.log("Privilages: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+      console.log("Privilages: " + emp.startDate);
+    }
+  }
+
+  // privilegesとstartDateが存在するため
+  printEmployeeInformation(e1);
+  printEmployeeInformation({
+    name: "Manu",
+    startDate: new Date(),
+  });
+
+  class Car {
+    drive() {
+      console.log("運転中...");
+    }
+  }
+
+  class Truck {
+    drive() {
+      console.log("トラック運転中...");
+    }
+
+    loadCargo(amount: number) {
+      console.log("荷物を乗せています..." + amount);
+    }
+  }
+
+  type Vehicle = Car | Truck;
+
+  const v1 = new Car();
+  const v2 = new Truck();
+
+  function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+
+    // クラスをタプガードを行う場合はinstanceofを使うことによってクラスのチェックを行うことができる。
+    if (vehicle instanceof Truck) {
+      vehicle.loadCargo(1000);
+    }
+  }
+
+  useVehicle(v1);
+  useVehicle(v2);
+}
