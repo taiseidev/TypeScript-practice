9e337f4d4214f607055d886aa04cb8f79fdf8cb4
onishi taisei <taisei.developer@gmail.com>
2023-01-17 21:15:38 +0900

    ジェネリクス型の基本について学習

--- dist/app.js
+++ dist/app.js
@@ -1,73 +1,7 @@
 "use strict";
-{
-    const e1 = {
-        name: "Max",
-        privileges: ["create-server"],
-        startDate: new Date(),
-    };
-    function add(a, b) {
-        if (typeof a === "string" || typeof b === "string") {
-            return a.toString() + b.toString();
-        }
-        return a + b;
-    }
-    add(12, 12);
-    function printEmployeeInformation(emp) {
-        console.log(emp.name);
-        if ("privileges" in emp) {
-            console.log("Privilages: " + emp.privileges);
-        }
-        if ("startDate" in emp) {
-            console.log("Privilages: " + emp.startDate);
-        }
-    }
-    printEmployeeInformation(e1);
-    printEmployeeInformation({
-        name: "Manu",
-        startDate: new Date(),
-    });
-    class Car {
-        drive() {
-            console.log("運転中...");
-        }
-    }
-    class Truck {
-        drive() {
-            console.log("トラック運転中...");
-        }
-        loadCargo(amount) {
-            console.log("荷物を乗せています..." + amount);
-        }
-    }
-    const v1 = new Car();
-    const v2 = new Truck();
-    function useVehicle(vehicle) {
-        vehicle.drive();
-        if (vehicle instanceof Truck) {
-            vehicle.loadCargo(1000);
-        }
-    }
-    useVehicle(v1);
-    useVehicle(v2);
-    function moveAnimal(animal) {
-        let speed;
-        switch (animal.type) {
-            case "bird":
-                speed = animal.flyingSpeed;
-                break;
-            case "hourse":
-                speed = animal.runnningSpeed;
-                break;
-        }
-        console.log("移動速度： " + speed);
-    }
-    moveAnimal({
-        type: "bird",
-        flyingSpeed: 10,
-    });
-    const userInputElement = document.getElementById("user-input");
-    if (userInputElement) {
-        userInputElement.value = "こんにちは";
-    }
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
 }
+const mergeObj = merge({ name: "Max" }, { age: 30 });
+console.log(mergeObj.age);
 //# sourceMappingURL=app.js.map
